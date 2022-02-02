ddd214bbdcd867b4d4c18ad5c0579f8aaa9322e6
barami62 <wodbs7758@naver.com>
2022-02-03 00:31:11 +0900

    시작

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from "react";
+import { Text, View } from "react-native";
+
+const App: FC = () => {
+
+  return (
+    <View>
+      <Text>
+        Hello World!
+      </Text>
+    </View>
+  );
+};
+
+export default App;
