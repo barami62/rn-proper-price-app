cfad99d16f92993d77dc07612c3cdd84d14643db
barami62 <wodbs7758@naver.com>
2022-02-03 19:49:02 +0900

    탭 2개 및 이미지 등록

---
+++ src/images.js
@@ -0,0 +1,9 @@
+import appIcon from '../assets/appIcon.png';
+import calculIcon from '../assets/calcul.png'
+import infoIcon from '../assets/info.png'
+
+export const images = {
+  appIcon,
+  calculIcon,
+  infoIcon
+};
--- src/navigations/TabNavigation.tsx
+++ src/navigations/TabNavigation.tsx
@@ -1,14 +1,34 @@
 import React, { FC } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import MainScreen from "../screens/MainScreen";
+import { Image } from "react-native";
+import { images } from "../images";
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 const TabNavigation: FC = () => {
+  const { calculIcon, infoIcon } = images;
 
   return (
     <Navigator>
-      <Screen name="Main" component={MainScreen} />
+      <Screen
+        name="Main"
+        component={MainScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Image source={calculIcon} />
+          )
+        }}
+      />
+      <Screen
+        name="Info"
+        component={MainScreen}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Image source={infoIcon} />
+          )
+        }}
+      />
     </Navigator>
   );
 };
