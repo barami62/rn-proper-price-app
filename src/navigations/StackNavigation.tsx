fa9c1eb042fdfa518e1769f6fe655f247a9e4ab1
barami62 <wodbs7758@naver.com>
2022-02-07 15:57:26 +0900

    커스텀 헤더 추가

--- src/navigations/StackNavigation.tsx
+++ src/navigations/StackNavigation.tsx
@@ -2,10 +2,21 @@ import React, { FC } from "react";
 import MainScreen from "../screens/MainScreen";
 import { createStackNavigator } from "@react-navigation/stack";
 import SearchScreen from "../screens/SearchScreen";
+import { Alert, Button, Image, Text, TextInput, View } from "react-native";
+import { images } from "../images";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/reducer";
 
 const { Navigator, Screen } = createStackNavigator();
 
 const StackNavigation: FC = () => {
+
+  const SearchHeader = () => {
+    const { contentBackgroundColor } = useSelector((state: RootState) => state.themes.LIGHT_MODE);
+    return (
+      <TextInput style={{ flex: 1, backgroundColor: contentBackgroundColor, width: 300, borderRadius: 12, justifyContent: 'center', paddingLeft: 12 }} />
+    )
+  }
 
   return (
     <Navigator>
@@ -19,6 +30,11 @@ const StackNavigation: FC = () => {
       <Screen
         name="Search"
         component={SearchScreen}
+        options={{
+          headerTitle: () => <SearchHeader />,
+          headerBackTitleVisible: false,
+          headerShadowVisible: true
+        }}
       />
     </Navigator>
   );
